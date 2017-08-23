2ac8edd2e38a213bd4310db7d666f4151aa0e3bd
papacheco <pedro.pacheco.a@gmail.com>
2017-08-23 16:23:08 -0300

    Validação MMC

--- 020/mmc.py
+++ 020/mmc.py
@@ -7,48 +7,30 @@ Calcula o MMC de um número inteiro
 
 '''
 
-def main():
-
-    n = int(input("Digite um numero (>1): "))
-
-    fator = 2 # primeiro fator
-    while n != 1:
-        # conta a multiplicidade de fator em n
-        mult = 0;
-        while n%fator == 0:
-            n = n / fator;
-            mult = mult + 1;
-
-        # imprime a multiplicade do fator
-        if mult != 0:
-            print("fator %d multiplicidade %d" %(fator, mult))
-
-        fator = fator + 1
-
-#-------------------------------------------------------
-main() # chamada da função principal
-
-"""
 def mmc():
     try:
         n = int(input("Digite um numero (>1): "))
-        fator = 2 # primeiro fator
+
+        fator = 2  # primeiro fator
         while n != 1:
+            # conta a multiplicidade de fator em n
             mult = 0;
-            while n%fator == 0:
+            while n % fator == 0:
                 n = n / fator;
                 mult = mult + 1;
-                if mult != 0:
-                    print("fator %d multiplicidade %d" %(fator, mult))
-            #print("%d", "%d" % (fator,mult))400
-                    fator = fator + 1
+
+            # imprime a multiplicade do fator
+            if mult != 0:
+                print("fator %d multiplicidade %d" % (fator, mult))
+
+            fator = fator + 1
+
     except Exception:
-        print('Verifique se está digitando números inteiros!')
+        print('Verifique se está digitando números INTEIROS!')
 
+mmc()
 
-mmc() # chamada da função principal
-
-
+"""
 class Testmmc(unittest.TestCase):
     def test_mmc(self):
         resultados = (['fator 5 multiplicidade 1','fator 7 multiplicidade 1',
