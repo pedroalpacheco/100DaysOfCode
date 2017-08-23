c854293e57554f6d1d718fea4a27ce7aa56cf6b1
papacheco <pedro.pacheco.a@gmail.com>
2017-08-23 17:09:59 -0300

    Com teste e verificação de função MMC

--- 020/mmc.py
+++ 020/mmc.py
@@ -2,26 +2,24 @@ import unittest
 '''
 Calcula o MMC de um número inteiro
 
--Fazer testes da função;
--Fazer validação da entrada de dados;
-
 '''
 
-def mmc():
+def mmc(n):
     try:
-        n = int(input("Digite um numero (>1): "))
 
-        fator = 2  # primeiro fator
+        fator = 2
         while n != 1:
-            # conta a multiplicidade de fator em n
+
             mult = 0;
             while n % fator == 0:
                 n = n / fator;
                 mult = mult + 1;
 
-            # imprime a multiplicade do fator
+
             if mult != 0:
-                print("fator %d multiplicidade %d" % (fator, mult))
+                return fator
+                #print(fator)
+
 
             fator = fator + 1
 
@@ -30,14 +28,10 @@ def mmc():
 
 mmc()
 
-"""
+
 class Testmmc(unittest.TestCase):
     def test_mmc(self):
-        resultados = (['fator 5 multiplicidade 1','fator 7 multiplicidade 1',
-                     'fator 13 multiplicidade 1',
-                     'fator 29 multiplicidade 1'])
-        self.assertEqual(mmc(13195), resultados)
+        self.assertEqual(mmc(4), 2)
 
 if __name__ == '__main__':
     unittest.main()
-"""
