d0dcbac8b4c2ad012832e97437acdb04e13f139d
papacheco <pedro.pacheco.a@gmail.com>
2017-08-24 15:06:36 -0300

    Potencializa ao quadrado o range do numero

---
+++ 021/diferenca_da_soma.py
@@ -0,0 +1,20 @@
+'''
+
+- A soma dos quadrados dos cem primeiros números naturais é?
+- O quadrado da soma dos cem primeiros números naturais é?
+- Daí a diferença entre a soma dos quadrados dos cem primeiros números naturais e o quadrado da soma?
+
+'''
+
+#A soma dos quadrados dos cem primeiros números naturais é?
+
+numeros = range(1, 4)
+
+
+#Potencializa ao quadrado os numeros do range
+for num in numeros:
+    quadrado = num ** 2
+    print(quadrado)
+#Falta somar!
+
+#------------------------------------------------------------
