b58f4b03c14a7baf4bb8c0af27bb14374eb070bf
papacheco <pedro.pacheco.a@gmail.com>
2017-08-25 11:19:20 -0300

    ler log

--- 022/fabfile.py
+++ 022/fabfile.py
@@ -1,10 +1,13 @@
 from fabric.api import run
 
-def host_type():#aqui é o comando que será executadp na linha de comando
+def host_type():#aqui é o comando que será executado na linha de comando
     run('uname -a')
 
 def horas():
     run('date')
 
 def regras_firewall():
-    run('sudo /sbin/iptables -nvL')
+    run('sudo /sbin/iptables -nvL ')
+
+def log_erro():
+    run('cat /var/log/apache2/erro.log')
