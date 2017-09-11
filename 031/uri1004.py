1d140799d9a6a8891078b118788ea2953f77b5a8
papacheco <pedro.pacheco.a@gmail.com>
2017-09-11 17:47:47 -0300

    inicio

---
+++ 031/uri1004.py
@@ -0,0 +1,8 @@
+#https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+
+a = int(input())
+b = int(input())
+
+PROD = a * b
+
+print('PROD = {}'.format(PROD))
---
+++ 032/uri1103.py
@@ -0,0 +1,24 @@
+
+h1 = [21,33]
+h2 = [1,36]
+
+#teste até 24 hrs
+while h1[0] <= '24' and h1[0] >= '0' and h2[0] <= '24' and h2[0] >= '0':
+
+
+"""
+if h1[0] <= 24 :
+
+if h2[0] <= 24:
+
+h1min = h1[0] * 60
+h1mintot = h1min + h1[1]
+
+h2min = h2[0] * 60
+h2mintot = h2min + h2[1]
+
+total = h2mintot - h1mintot
+
+print(total)
+
+"""
