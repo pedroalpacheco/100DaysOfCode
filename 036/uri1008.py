34f009211e54cf3802840a11c4495d50d137edca
papacheco <pedro.pacheco.a@gmail.com>
2017-09-12 15:53:58 -0300

    uri 1008

---
+++ 036/uri1008.py
@@ -0,0 +1,11 @@
+nrempregado = 6
+#nrempregado = int(input())
+horastrab = 145
+#horastrab = int(input())
+valorhora = 15.55
+#valorhora = float(input())
+
+salario = valorhora * horastrab
+
+print('NUMBER = {}'.format(nrempregado))
+print('SALARY = U$ {:.2f}'.format(salario))
