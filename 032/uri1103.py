342e846a55408dc086bcaf25e5a2e1cb3313667f
papacheco <pedro.pacheco.a@gmail.com>
2017-09-12 09:46:55 -0300

    uri 1103

--- 032/uri1103.py
+++ 032/uri1103.py
@@ -1,24 +1,17 @@
+def Main():
+    while True:
+        H1, M1, H2, M2 = map(int, input().split())
+        if H1 == M1 == H2 == M2 == 0:
+            break
+        else:
+            x = H1 * 60 + M1
+            y = H2 * 60 + M2
 
-h1 = [21,33]
-h2 = [1,36]
+            if (y <= x):
+                y += 24 * 60
 
-#teste até 24 hrs
-while h1[0] <= '24' and h1[0] >= '0' and h2[0] <= '24' and h2[0] >= '0':
+            print(abs(y - x))
 
 
-"""
-if h1[0] <= 24 :
-
-if h2[0] <= 24:
-
-h1min = h1[0] * 60
-h1mintot = h1min + h1[1]
-
-h2min = h2[0] * 60
-h2mintot = h2min + h2[1]
-
-total = h2mintot - h1mintot
-
-print(total)
-
-"""
+if __name__ == '__main__':
+    Main()
