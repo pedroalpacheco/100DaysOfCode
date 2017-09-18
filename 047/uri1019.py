58446a4520bae3341a499afed46d747efa3dff49
papacheco <pedro.pacheco.a@gmail.com>
2017-09-18 10:40:52 -0300

    Melhorado - uri 1019

--- 047/uri1019.py
+++ 047/uri1019.py
@@ -1,9 +1,25 @@
 
-segundos = int(input())
+import unittest
 
-hora = segundos / 3600
-segundos %= 3600
-minutes = segundos / 60
-segundos %= 60
-seconds = segundos
-print("%d:%d:%d" % (hora, minutes, seconds))
+def converte_segundos(segundos):
+    segundos = int(input())
+
+    hora = segundos / 3600
+    segundos %= 3600
+    minutes = segundos / 60
+    segundos %= 60
+    seconds = segundos
+    return "%d:%d:%d" % (hora, minutes, seconds)
+
+
+# testes ------
+class TestConversao(unittest.TestCase):
+
+    def test_segundos(self):
+        self.assertTrue(converte_segundos(140153), '38:55:53')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
