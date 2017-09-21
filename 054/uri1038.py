012199264e498e9f01b34e8a8ec2889ec6a5d022
papacheco <pedro.pacheco.a@gmail.com>
2017-09-21 11:56:43 -0300

    uri 1038 - limpo

--- 054/uri1038.py
+++ 054/uri1038.py
@@ -25,26 +25,3 @@ def menu():
 
 menu()
 
-"""
-pedidos = input().split(' ')
-
-a, b = pedidos
-
-if a == '1':
-    valor = float(b) * 4.00
-    print('Total: R$ {:.2f}'.format(valor))
-elif a == '2':
-    valor = float(b) * 4.50
-    print('Total: R$ {:.2f}'.format(valor))
-elif a == '3':
-    valor = float(b) * 5.00
-    print('Total: R$ {:.2f}'.format(valor))
-elif a == '4':
-    valor = float(b) * 2.00
-    print('Total: R$ {:.2f}'.format(valor))
-elif a == '5':
-    valor = float(b) * 1.50
-    print('Total: R$ {:.2f}'.format(valor))
-else:
-    print('Codigo não encontrado!')
-"""
