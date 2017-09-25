0af1b4c00f2ecafdc08579ea619ddc37c37e02af
papacheco <pedro.pacheco.a@gmail.com>
2017-09-25 15:47:16 -0300

    uri 1040 - Funcionand

---
+++ 056/uri1041.py
@@ -0,0 +1,36 @@
+x, y = (float(x) for x in input("").split())
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+elif x > 0 and y > 0:
+    print("Q1")
+elif x < 0 and y > 0:
+    print("Q2")
+elif x < 0 and y < 0:
+    print("Q3")
+else:
+    print("Q4")
+
+"""
+coordenadas = input().split(' ')
+x, y = coordenadas
+
+if float(x) == 0 and float(y) == 0:
+    print('Origem')
+elif x == 0:
+    print("Eixo Y")
+elif y == 0:
+    print("Eixo X")
+elif float(x) > 0 and float(y) > 0:
+    print('Q1')
+elif float(x)<0 and float(y) > 0:
+    print('Q2')
+elif float(x) < 0 and float(y) < 0:
+    print('Q3')
+else:
+    print('Q4')
+"""
